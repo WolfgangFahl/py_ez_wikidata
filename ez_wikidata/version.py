e2f97bbf08ee1ef8ae677679983c2140c40c01cb
Wolfgang Fahl <wf@bitplan.com>
2024-08-09 15:39:19 +0200

    release commit

--- ez_wikidata/version.py
+++ ez_wikidata/version.py
@@ -17,7 +17,7 @@ class Version(object):
     name = "py_ez_wikidata"
     version = ez_wikidata.__version__
     date = "2024-03-01"
-    updated = "2024-05-19"
+    updated = "2024-08-09"
     description = "Mapping for Wikidata allows creation of wikidata entries from dicts"
 
     authors = "Tim Holzheim, Wolfgang Fahl"
